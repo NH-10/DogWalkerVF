be327ea44e021c3075557aa4bcbdf8a49c2e6b52
NH-10 <natalieh101101@gmail.com>
2021-10-20 20:03:23 -0500

    v1

---
+++ src/main/java/pe/edu/upc/spring/DogWalkerV2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogWalkerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogWalkerV2Application.class, args);
+	}
+
+}//holaaaa
