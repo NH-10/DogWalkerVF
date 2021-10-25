6da6f64ab86710923cfbe382987f918a73e9d232
KellyHT <u201915358@upc.edu.pe>
2021-10-25 14:07:00 -0500

    irPaginaLoginOwner -irPaginaLoginWalker

--- src/main/java/pe/edu/upc/spring/controller/LoginController.java
+++ src/main/java/pe/edu/upc/spring/controller/LoginController.java
@@ -25,6 +25,15 @@ public class LoginController {
 		return "login";
 	}
 	
+	@RequestMapping("/loginOwner")
+	public String irPaginaLoginOwner() {
+		return "loginOwner";
+	}
+	@RequestMapping("/loginWalker")
+	public String irPaginaLoginWalker() {
+		return "loginWalker";
+	}
+	
 	
 	
 
