5ee91d266ccecb39c1b53370de8d565801888413
KellyHT <u201915358@upc.edu.pe>
2021-10-25 12:48:28 -0500

    borrando import

--- src/main/java/pe/edu/upc/spring/controller/LoginController.java
+++ src/main/java/pe/edu/upc/spring/controller/LoginController.java
@@ -1,29 +1,11 @@
 package pe.edu.upc.spring.controller;
 
-import java.util.Map;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.sun.el.parser.ParseException;
-
-import pe.edu.upc.spring.model.Owner;
-import pe.edu.upc.spring.model.ServiceRequest;
-import pe.edu.upc.spring.model.Status;
-import pe.edu.upc.spring.model.Time;
-import pe.edu.upc.spring.model.Walker;
 import pe.edu.upc.spring.service.IOwnerService;
 import pe.edu.upc.spring.service.IServiceRequestService;
-import pe.edu.upc.spring.service.IStatusService;
-import pe.edu.upc.spring.service.ITimeService;
 import pe.edu.upc.spring.service.IWalkerService;
 
 @Controller
