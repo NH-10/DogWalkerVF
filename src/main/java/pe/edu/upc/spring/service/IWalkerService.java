218fa3c08245a4b646bec1e065f3c03cfbda919d
KellyHT <u201915358@upc.edu.pe>
2021-10-25 20:12:41 -0500

    añadir funcion findByEmailAndPassword en service

--- src/main/java/pe/edu/upc/spring/service/IWalkerService.java
+++ src/main/java/pe/edu/upc/spring/service/IWalkerService.java
@@ -7,5 +7,7 @@ import pe.edu.upc.spring.model.Walker;
 public interface IWalkerService {
 	public boolean save(Walker walker);
 	public List<Walker> list();
+	public List<Walker> findByEmailAndPassword(String email, String password);
+
 
 }
