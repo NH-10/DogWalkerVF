fe69991a54e17b1405d53a0b4ef094c1de1c4a3d
KellyHT <u201915358@upc.edu.pe>
2021-10-25 20:11:37 -0500

    ordenar codigo

--- src/main/java/pe/edu/upc/spring/controller/OwnerController.java
+++ src/main/java/pe/edu/upc/spring/controller/OwnerController.java
@@ -18,138 +18,116 @@ import pe.edu.upc.spring.service.IDogService;
 import pe.edu.upc.spring.service.IOwnerService;
 
 @Controller
-@RequestMapping("/owner") 
+@RequestMapping("/owner")
 public class OwnerController {
 	@Autowired
 	private IOwnerService oService;
-	
+
 	@Autowired
 	private IDistrictService dService;
-	
+
 	@Autowired
 	private IDogService cService;
-	
+
 	private Owner sesionOwner;
-	
+
 	@RequestMapping("/inicio")
 	public String irPaginaInicio() {
 		return "bienvenido";
 	}
-	
-	
+
 	@RequestMapping("/bienvenido")
 	public String irPaginaBienvenida(Model model) {
 		model.addAttribute("owner", sesionOwner);
-		return "bienvenidoOwner"; 
+		return "bienvenidoOwner";
 	}
-	
 
-	
 	@RequestMapping("/menu")
 	public String irMenuOwner() {
 		return "OwnerMenu";
 	}
-	
+
 	@RequestMapping("/loginOwner")
 	public String irPaginaLoginOwner() {
 		return "loginOwner";
 	}
-	
-	
+
 	@RequestMapping("/irRegistrar")
 	public String irPaginaRegistrar(Model model) {
 		model.addAttribute("owner", new Owner());
 		model.addAttribute("listadistrito", dService.listDistrict());
-		return "owner"; 
+		return "owner";
 	}
-	
+
 	@RequestMapping("/registrar")
-	public String registrar(@ModelAttribute Owner objOwner, BindingResult binRes, Model model) 
-		throws ParseException
-	{
-		if (binRes.hasErrors())
-		{
+	public String registrar(@ModelAttribute Owner objOwner, BindingResult binRes, Model model) throws ParseException {
+		if (binRes.hasErrors()) {
 			model.addAttribute("listadistrito", dService.listDistrict());
 			return "owner";
-		}
-		else {
+		} else {
 			boolean flag = oService.save(objOwner);
 			if (flag) {
-				sesionOwner=objOwner;
+				sesionOwner = objOwner;
 				return "redirect:/owner/bienvenido";
-			}
-			else {
-				model.addAttribute("mensaje","Ocurrio un error");
+			} else {
+				model.addAttribute("mensaje", "Ocurrio un error");
 				return "redirect:/owner/irRegistrar";
 			}
 		}
 	}
-	
-	
+
 	@RequestMapping("/modificar")
-	public String modificar(Model model)  {
+	public String modificar(Model model) {
 		model.addAttribute("owner", sesionOwner);
 		model.addAttribute("listadistrito", dService.listDistrict());
 		return "ownerEdit";
-		
+
 	}
-	
-	
-		
+
 	@RequestMapping("/irBuscar")
-	public String IrBuscar(Model model) 
-	throws ParseException
-	{
+	public String IrBuscar(Model model) throws ParseException {
 		model.addAttribute("owner", new Owner());
 		return "buscar";
 	}
-			
+
 	@RequestMapping("/buscar")
-	public String buscar(Map<String,Object>model, @ModelAttribute Owner owner) 
-	throws ParseException
-	{
-		List<Owner>listOwners;
+	public String buscar(Map<String, Object> model, @ModelAttribute Owner owner) throws ParseException {
+		List<Owner> listOwners;
 		owner.setFirstNames(owner.getFirstNames());
-		listOwners= oService.findByName(owner.getFirstNames());
-				
-		if(listOwners.isEmpty()) {
+		listOwners = oService.findByName(owner.getFirstNames());
+
+		if (listOwners.isEmpty()) {
 			model.put("mensaje", "No existen coincidencias");
 		}
-				
+
 		model.put("listOwners", listOwners);
-				
+
 		return "buscar";
 	}
-	
+
 	@RequestMapping("/listarCanes")
-	public String listarCanes(Map<String,Object>model, @ModelAttribute Owner owner) 
-	throws ParseException
-	{
+	public String listarCanes(Map<String, Object> model, @ModelAttribute Owner owner) throws ParseException {
 		cService.ListDogByOwner(owner.getIdOwner());
 		return "listCanes";
 	}
-	
-	
+
 	@RequestMapping("/validarUsuario")
-	public String ingresarCuenta(@ModelAttribute Owner objOwner) 
-	throws ParseException
-	{
-		List<Owner>listOwners;
+	public String ingresarCuenta(@ModelAttribute Owner objOwner) throws ParseException {
+		List<Owner> listOwners;
 		objOwner.setEmail(objOwner.getEmail());
 		objOwner.setPassword(objOwner.getPassword());
-		listOwners= oService.findByEmailAndPassword(objOwner.getEmail(),objOwner.getPassword());
-				
-		if(!listOwners.isEmpty()) {
+		listOwners = oService.findByEmailAndPassword(objOwner.getEmail(), objOwner.getPassword());
+
+		if (!listOwners.isEmpty()) {
 			objOwner = listOwners.get(0);
 			System.out.print(objOwner.getEmail());
 			System.out.print(objOwner.getFirstNames());
 
-			sesionOwner=objOwner;
+			sesionOwner = objOwner;
 			return "redirect:/owner/bienvenido";
 		}
-				
+
 		return "loginOwner";
 	}
-	
 
 }
