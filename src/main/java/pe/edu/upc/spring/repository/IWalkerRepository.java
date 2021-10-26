18bd0d52e394d3467c0ad6953fccd0ac8458f799
KellyHT <u201915358@upc.edu.pe>
2021-10-25 20:12:24 -0500

    repository funcion findByEmailAndPassword

--- src/main/java/pe/edu/upc/spring/repository/IWalkerRepository.java
+++ src/main/java/pe/edu/upc/spring/repository/IWalkerRepository.java
@@ -1,7 +1,11 @@
 package pe.edu.upc.spring.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.spring.model.Walker;
@@ -9,5 +13,8 @@ import pe.edu.upc.spring.model.Walker;
 
 @Repository
 public interface IWalkerRepository extends JpaRepository<Walker, Integer>{
+	
+	@Query("from Walker w where w.email like %:email% and w.password like %:password% ")
+	List<Walker> findByEmailAndPassword(@Param("email")String email, @Param("password")String password);
 
 }
