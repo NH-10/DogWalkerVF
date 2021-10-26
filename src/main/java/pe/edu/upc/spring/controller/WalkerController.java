14face76100dd73c77ecad86c99ca86d6e8078cb
KellyHT <u201915358@upc.edu.pe>
2021-10-25 20:12:03 -0500

    añadir funcion de ingresarCuenta y cambios en bienvenido

--- src/main/java/pe/edu/upc/spring/controller/WalkerController.java
+++ src/main/java/pe/edu/upc/spring/controller/WalkerController.java
@@ -1,94 +1,104 @@
 package pe.edu.upc.spring.controller;
 
-
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sun.el.parser.ParseException;
 
-import pe.edu.upc.spring.model.Owner;
 import pe.edu.upc.spring.model.Walker;
-
 import pe.edu.upc.spring.service.IDistrictService;
 import pe.edu.upc.spring.service.IPersonalityService;
 import pe.edu.upc.spring.service.IWalkerService;
 
-
 @Controller
-@RequestMapping("/walker") 
+@RequestMapping("/walker")
 public class WalkerController {
-	
+
 	@Autowired
 	private IDistrictService dService;
-	
+
 	@Autowired
 	private IPersonalityService pService;
-	
+
 	@Autowired
 	private IWalkerService wService;
-	
 
 	private Walker sesionWalker;
-	
+
 	@RequestMapping("/inicio")
 	public String irPaginaInicio() {
 		return "bienvenido";
 	}
+
 	@RequestMapping("/bienvenido")
-	public String irPaginaBienvenida() {
+	public String irPaginaBienvenida(Model model) {
+		model.addAttribute("walker", sesionWalker);
 		return "bienvenidoWalker";
 	}
-	
+
 	@RequestMapping("/menu")
 	public String irMenuWalker() {
 		return "menuWalker";
 	}
-	
-	@RequestMapping("/irRegistrar")  // función registrar paseador
+
+	@RequestMapping("/irRegistrar") // función registrar paseador
 	public String irPaginaRegistrar(Model model) {
 		model.addAttribute("walker", new Walker());
 		model.addAttribute("listadistrito", dService.listDistrict());
 		model.addAttribute("listpersonalidad", pService.listPersonality());
-		return "walker"; 
+		return "walker";
 	}
-	
+
 	@RequestMapping("/registrar")
-	public String registrar(@ModelAttribute Walker objWalker, BindingResult binRes, Model model) 
-		throws ParseException
-	{
-		if (binRes.hasErrors())
-		{
+	public String registrar(@ModelAttribute Walker objWalker, BindingResult binRes, Model model) throws ParseException {
+		if (binRes.hasErrors()) {
 			model.addAttribute("listadistrito", dService.listDistrict());
 			model.addAttribute("listpersonalidad", pService.listPersonality());
 			return "walker";
-		}
-		else {
+		} else {
 			boolean flag = wService.save(objWalker);
 			if (flag) {
-				sesionWalker= objWalker;
+				sesionWalker = objWalker;
 				return "redirect:/walker/bienvenido";
-			}
-			else {
-				model.addAttribute("mensaje","Ocurrio un error");
+			} else {
+				model.addAttribute("mensaje", "Ocurrio un error");
 				return "redirect:/walker/irRegistrar";
 			}
 		}
 	}
-	
+
 	@RequestMapping("/modificar")
-	public String modificar(Model model)  {
+	public String modificar(Model model) {
 		model.addAttribute("walker", sesionWalker);
 		model.addAttribute("listadistrito", dService.listDistrict());
 		model.addAttribute("listpersonalidad", pService.listPersonality());
 		return "walkerEdit";
-		
+
+	}
+	
+	
+	@RequestMapping("/validarUsuario")
+	public String ingresarCuenta(@ModelAttribute Walker objWalker) throws ParseException {
+		List<Walker> listWalkers;
+		objWalker.setEmail(objWalker.getEmail());
+		objWalker.setPassword(objWalker.getPassword());
+		listWalkers = wService.findByEmailAndPassword(objWalker.getEmail(), objWalker.getPassword());
+
+		if (!listWalkers.isEmpty()) {
+			objWalker = listWalkers.get(0);
+			System.out.print(objWalker.getEmail());
+			System.out.print(objWalker.getFirstNames());
+
+			sesionWalker = objWalker;
+			return "redirect:/walker/bienvenido";
+		}
+
+		return "loginWalker";
 	}
 }
