bb8cc90b4c40f0f566cbfb9923fbe9a3c8659286
KellyHT <u201915358@upc.edu.pe>
2021-11-10 15:46:35 -0500

    quitando imports que no se usan

--- src/main/java/pe/edu/upc/spring/controller/FeedbackController.java
+++ src/main/java/pe/edu/upc/spring/controller/FeedbackController.java
@@ -2,32 +2,22 @@ package pe.edu.upc.spring.controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sun.el.parser.ParseException;
 
 import pe.edu.upc.spring.model.District;
-import pe.edu.upc.spring.model.Dog;
 import pe.edu.upc.spring.model.Feedback;
 import pe.edu.upc.spring.model.Owner;
-import pe.edu.upc.spring.model.ServiceRequest;
 import pe.edu.upc.spring.model.Walker;
-import pe.edu.upc.spring.service.IDistrictService;
-import pe.edu.upc.spring.service.IDogService;
 import pe.edu.upc.spring.service.IFeedbackService;
-import pe.edu.upc.spring.service.IOwnerService;
-import pe.edu.upc.spring.service.IRaceService;
-import pe.edu.upc.spring.service.IServiceRequestService;
 import pe.edu.upc.spring.service.IWalkerService;
 
 @Controller
