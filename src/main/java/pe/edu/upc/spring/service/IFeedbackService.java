3b41b967e879943fe716fe700d370879424a8e20
KellyHT <u201915358@upc.edu.pe>
2021-11-10 15:52:46 -0500

    quitando imports que no se usan

--- src/main/java/pe/edu/upc/spring/service/IFeedbackService.java
+++ src/main/java/pe/edu/upc/spring/service/IFeedbackService.java
@@ -1,11 +1,8 @@
 package pe.edu.upc.spring.service;
 
 import java.util.List;
-import java.util.Optional;
 
-import pe.edu.upc.spring.model.Dog;
 import pe.edu.upc.spring.model.Feedback;
-import pe.edu.upc.spring.model.Walker;
 
 
 public interface IFeedbackService {
