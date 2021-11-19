3139ed702560eba9ef0ae497dc1c2bf330e3f519
KellyHT <u201915358@upc.edu.pe>
2021-11-19 12:43:35 -0500

    limpiando codigo

--- src/main/java/pe/edu/upc/spring/controller/LoginController.java
+++ src/main/java/pe/edu/upc/spring/controller/LoginController.java
@@ -3,8 +3,6 @@ package pe.edu.upc.spring.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,23 +14,16 @@ import pe.edu.upc.spring.model.Owner;
 import pe.edu.upc.spring.model.Walker;
 import pe.edu.upc.spring.service.IOwnerService;
 import pe.edu.upc.spring.service.IWalkerService;
-import pe.edu.upc.spring.serviceimpl.JpaUserDetailsService;
 
 @Controller
 @RequestMapping
 public class LoginController {
 	
 	@Autowired
-	private JpaUserDetailsService userDetailsService;
-	
-	@Autowired
 	private IOwnerService oService;
 	
 	@Autowired
 	private IWalkerService wService;
-	
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private OwnerController oController;
